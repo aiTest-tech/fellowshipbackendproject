d3687f9a657e6d73620908dcb2f6a039e4fba4d3
aiTest-tech <cmoaiteam@gmail.com>
2024-12-17 16:46:14 +0530

    admin panel added, model change

--- fellowshipbackendproject/app/admin.py
+++ fellowshipbackendproject/app/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import MediaModel
 # Register your models here.
 
-admin.site.register(MediaModel)
+class MediaModelAdmin(admin.ModelAdmin):
+    # List of fields to display in the admin list view
+    list_display = ('id', 'title_eng', 'title_guj', 'img', 'active_deactivate')
+
+    # Optional: Add filters for boolean fields or others
+    list_filter = ('active_deactivate',)
+
+    # Optional: Add search functionality
+    search_fields = ('title_eng', 'title_guj')
+
+# Register the model with the custom admin class
+admin.site.register(MediaModel, MediaModelAdmin)
--- fellowshipbackendproject/app/models.py
+++ fellowshipbackendproject/app/models.py
@@ -4,5 +4,14 @@ from base.models import BaseModel
 
 # Create your models here.
 class MediaModel(BaseModel):
-    img = models.ImageField(upload_to="media/")
-    title = models.TextField()
+    # img = models.ImageField(upload_to="media/")
+    # title = models.TextField()
+    # active    
+    title_eng = models.TextField()
+    img = models.ImageField(upload_to='media/')
+    title_guj = models.TextField()
+    active_deactivate = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title_eng
